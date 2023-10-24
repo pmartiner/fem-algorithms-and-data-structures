9f5cb5a9ae94ca22392e9dc686419f0f5b643bc2
Pablo Martínez <pablo.martinez94@proton.me>
2023-10-24 13:52:29 -0600

    feat: binary search comparison algorithm added

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,4 +1,27 @@
 export default function compare(
   a: BinaryNode<number> | null,
   b: BinaryNode<number> | null,
-): boolean {}
+): boolean {
+  // Structural check:
+  // We check if both child nodes hit null (no children) at the same time,
+  // which would mean that they are structurally the same
+  if (a === null && b === null) {
+    return true;
+  }
+
+  // Structural check:
+  // We check if both child nodes hit null (no children) on different levels,
+  // which would mean that they are structurally different
+  if (a === null || b === null) {
+    return false;
+  }
+
+  // Value check:
+  // If we reached this condition, we know that the structure is the same
+  // and we want to know if their value's are also equal
+  if (a.value !== b.value) {
+    return false;
+  }
+
+  return compare(a.left, b.left) && compare(a.right, b.right);
+}
