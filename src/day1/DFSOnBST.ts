a331728448fd30016de0d09947e35e2bdf72de7f
Pablo Martínez <pablo.martinez94@proton.me>
2023-10-24 15:26:53 -0600

    feat: depth-first search algorithm added

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,4 +1,25 @@
-export default function dfs(
-  head: BinaryNode<number>,
+function search(
+  currentNode: BinaryNode<number> | null,
   needle: number,
-): boolean {}
+): boolean {
+  if (!currentNode) {
+    return false;
+  }
+
+  if (currentNode.value === needle) {
+    return true;
+  }
+
+  if (currentNode.value < needle) {
+    return search(currentNode.right, needle);
+  }
+
+  return search(currentNode.left, needle);
+}
+
+export default function dfs(
+  head: BinaryNode<number> | null,
+  needle: number,
+): boolean {
+  return search(head, needle);
+}
